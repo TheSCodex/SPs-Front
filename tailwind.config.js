2659b3d9cca9deb66a740e2ad7eb545deed029cd
ppeguero <frdpgr@gmail.com>
2024-03-12 10:09:16 -0500

    Añadí colores personalizados a taiwlind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,14 @@ module.exports = {
     extend: {
       fontFamily: {
         poppins: ['Poppins', 'sans-serif'],
+        outfit: ['Outfit', 'sans-serif']
       },
+      colors: {
+        primaryColor: '#21325E',
+        secondaryColor: '#3E497A',
+        focusColor: '#F1D00A',
+        contrastColor: '#F0F0F0',
+      }
     },
     },
     plugins: [],
