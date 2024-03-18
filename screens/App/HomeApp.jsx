543276442feb60c446eb38aea16c5688b53673ec
Alexander <alexdroide466@gmail.com>
2024-03-17 19:23:42 -0500

    Colocacion de la imagen

--- screens/App/HomeApp.jsx
+++ screens/App/HomeApp.jsx
@@ -10,10 +10,14 @@ export default function HomeApp() {
       <StatusBar backgroundColor="#21325E" />
       <BtnExit />
       <View className="space-x-2 flex-row bg-focusColor rounded-2xl w-[75%] h-12 justify-center items-center">
-        <Text className='text-white font-bold text-lg'>7</Text>
-        <Text className='text-white font-semibold text-lg'>Lugares disponibles</Text>
+        <Text className="text-white font-bold text-lg">7</Text>
+        <Text className="text-white font-semibold text-lg">
+          Lugares disponibles
+        </Text>
       </View>
-      <Parking />
+      <View className='w-full h-1/2'>
+        <Parking />
+      </View>
       <View className=" bg-secondaryColor w-full h-[22%] flex justify-center items-center rounded-t-3xl space-y-2">
         <TouchableOpacity className="border border-focusColor bg-primaryColor rounded">
           <Text className="text-white font-bold text-center px-20 py-4 text-base">
--- screens/App/Parking.jsx
+++ screens/App/Parking.jsx
@@ -1,41 +1,14 @@
 import { View, Text, StyleSheet, TouchableOpacity, Image } from "react-native";
 import React from "react";
-import img from "../../img/car.png";
+import img from "../../img/Parking.png";
 import { AntDesign } from "@expo/vector-icons";
-
 
 const Parking = () => {
   return (
-    <View className="flex-col items-center w-full">
+    <View className="flex-col items-center justify-center w-full">
       {/* Icono de entrada */}
-      <AntDesign name="down" size={30} color="grey" />
-      <View className="flex-row justify-around w-full">
-        {/* Cajon de estacionamiento 1 */}
-        <View className="space-y-3">
-          {Array.from({ length: 5 }, (_, i) => (
-            <TouchableOpacity
-              key={i}
-              style={styles.container}
-              className="flex items-center justify-center"
-            >
-              <Image source={img} className="w-10 h-20 -rotate-90" />
-              {/* <Text className='text-white'>{i+1}</Text> */}
-            </TouchableOpacity>
-          ))}
-        </View>
-        {/* Cajon de estacionamiento 2 */}
-        <View className="space-y-3 rotate-180">
-          {Array.from({ length: 5 }, (_, i) => (
-            <TouchableOpacity
-              key={i + 5}
-              style={styles.container}
-              className="flex items-center justify-center"
-            >
-              {/* <Image source={img} className="w-10 h-20 -rotate-90" /> */}
-              <Text className="text-white rotate-180">{i + 6}</Text>
-            </TouchableOpacity>
-          ))}
-        </View>
+      <View className='w-[72%] h-full'>
+        <Image source={img} className="w-full h-full" />
       </View>
     </View>
   );
